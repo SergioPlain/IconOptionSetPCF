b633cb62288e48ed2627673afa047324d651d399
SergioPlain <113335926+SergioPlain@users.noreply.github.com>
2023-04-18 00:37:44 +0200

    initial commit with all files for PCF

---
+++ IconSingleOptionSet/IconOptionSet.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import { useState, useEffect } from "react";
+
+import { initializeIcons, mergeStyles, FontIcon, TextField, IconButton,IIconProps, ChoiceGroup, IChoiceGroupOption, Stack} from "@fluentui/react";
+import { useConst } from '@fluentui/react-hooks'
+
+export interface IProps {
+    selected: number | undefined;
+    icons: Array<IIconSetup>;
+    readonly: boolean;
+    masked: boolean;
+    nullable: boolean;  
+    selectedcolor: string | undefined;
+    
+    
+    // eslint-disable-next-line no-unused-vars
+    onChange: (selected:number|undefined) => void;
+}
+
+export interface IIconSetup {
+    key : number
+    icon: string;
+    text: string;
+}
+
+export interface IState {
+    selectedvalue: string | undefined;
+}
+
+// eslint-disable-next-line no-undef
+export const IconOptionSetControl = (props : IProps): JSX.Element => {
+
+    //STATE VARIABLES
+    const [selectedValue, setSelectedValue] = useState<string|undefined>(props.selected == undefined ? undefined : props.selected.toString());
+    const [options, setOptions] = useState<IChoiceGroupOption[]>(() => {
+        return props.icons.map(iconsetup=>getChoiceGroupOption(iconsetup,props,selectedValue))
+    });
+    const [cancelIconProps] = useState<IIconProps>(() => {
+        return {iconName:"Cancel"};
+    });
+
+
+    //-Initialization : will happen only once = like a contructor
+    useConst(() => {
+        initializeIcons();
+    });
+
+    //EFFECT HOOKS
+    useEffect(() => {
+    
+        setSelectedValue(props.selected == undefined ? undefined : props.selected.toString());
+        setOptions(props.icons.map(iconsetup=>getChoiceGroupOption(iconsetup,props,selectedValue)))
+        
+    }, [props.selected]);
+
+
+    useEffect(() => {
+        selectedValue == undefined ? props.onChange(undefined) :
+                                    props.onChange(Number(selectedValue));       
+    }, [selectedValue]);
+
+    const onChange=(ev?: React.SyntheticEvent<HTMLElement>, option?: IChoiceGroupOption) => {
+        if(option != undefined)
+        {
+            setSelectedValue(option.key);            
+        } 
+    }
+
+    const onCancel = () => {
+        setSelectedValue(undefined);
+    }
+
+    
+
+    function getChoiceGroupOption(iconsetup:IIconSetup, props:IProps, selectedValue:string|undefined):IChoiceGroupOption {
+        let key:string = iconsetup.key.toString();
+        let choicegoupoption:IChoiceGroupOption = {key:key,
+                                                    text:iconsetup.text,
+                                                    disabled: props.readonly,
+                                                    iconProps:{
+                                                        iconName:iconsetup.icon,
+                                                        style:{
+                                                            color:key==selectedValue?
+                                                                        props.selectedcolor:
+                                                                        undefined
+                                                        }
+                                                    }
+                                                };
+        
+        return choicegoupoption;
+    }
+
+    //STYLES
+    const maskedclass = mergeStyles({
+        fontSize: 30,
+        height: 30,
+        width: 50,
+        margin: "1px",      
+    });
+
+    //RENDERING
+    if(props.icons.length > 5)
+    {
+        return <div>ERROR : this control is designed for 5 icons or less</div>
+    }else if(props.masked){
+        return(
+            <Stack tokens={{ childrenGap: 2 }} horizontal>
+                <FontIcon iconName="Lock" className={maskedclass} />     
+                <TextField value="*********" style={{width:"100%"}}/>
+            </Stack>
+        )
+    }else{
+    return (
+                <Stack horizontal>
+                    {props.nullable &&
+                        <IconButton 
+                            iconProps={cancelIconProps}
+                            title="Cancel" 
+                            ariaLabel="Cancel" 
+                            onClick={onCancel}
+                        />
+                    }
+
+                    <ChoiceGroup
+                        selectedKey={selectedValue}
+                        options = {options}
+                        onChange={onChange}
+                    />
+                </Stack>
+                
+            );
+        }
+
+}
---
+++ IconSingleOptionSet/index.ts
@@ -0,0 +1,125 @@
+/* eslint-disable no-unused-vars */
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import { IconOptionSetControl, IProps, IIconSetup} from "./IconOptionSet";
+import * as React from "react";
+
+export class IconSingleOptionSet implements ComponentFramework.ReactControl<IInputs, IOutputs> {
+    private _selected: number | undefined;
+    
+    private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
+    private _notifyOutputChanged: () => void;
+
+    private _props: IProps = {
+        selected: undefined,
+        icons: [],
+        readonly: false,
+        masked: false,
+        nullable: false,
+        selectedcolor: "",
+        onChange: this.notifyChange.bind(this)
+    }
+
+
+    notifyChange(selected: number|undefined) {
+		
+		this._selected = selected;
+		this._notifyOutputChanged();
+	}
+
+    /**
+     * Empty constructor.
+     */
+    constructor() { }
+
+    /**
+     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+     * Data-set values are not initialized here, use updateView.
+     * @param _context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+     * @param _state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+     */
+    public init(
+        _context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        _state: ComponentFramework.Dictionary
+    ): void {
+        this._notifyOutputChanged = notifyOutputChanged;
+    }
+
+    /**
+     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+     * @param _context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+     * @returns ReactElement root react element for the control
+     */
+    public updateView(_context: ComponentFramework.Context<IInputs>): React.ReactElement {
+        
+        // If the form is diabled because it is inactive or the user doesn't have access
+		// isControlDisabled is set to true
+		let isReadOnly = _context.mode.isControlDisabled;
+
+		let isMasked = false;
+		// When a field has FLS enabled, the security property on the attribute parameter is set
+		if (_context.parameters.optionset.security) {
+			isReadOnly = isReadOnly || !_context.parameters.optionset.security.editable;
+			isMasked =  !_context.parameters.optionset.security.readable
+		}
+
+		/*if(_context.parameters.optionset.attributes == null){
+			return;
+		}*/
+
+		this._selected = _context.parameters.optionset.raw !== null ? _context.parameters.optionset.raw : undefined;
+
+		let options:ComponentFramework.PropertyHelper.OptionMetadata[] 
+				= _context.parameters.optionset.attributes!.Options;
+
+		let icons:Array<string> = [_context.parameters.icon1.raw || "",
+									_context.parameters.icon2.raw || "",
+									_context.parameters.icon3.raw || "",
+									_context.parameters.icon4.raw || "",
+									_context.parameters.icon5.raw || ""];
+		
+		//Prepare Props for React Component
+		this._props.selected = this._selected;
+		this._props.icons = this.getIconSetups(options,icons);
+		this._props.selectedcolor = _context.parameters.selectedcolor.raw || "";
+		this._props.readonly = isReadOnly;
+		this._props.masked = isMasked;
+		this._props.nullable = true; //todo add as a parameter in mamnifest
+
+
+        return React.createElement(
+            IconOptionSetControl, this._props
+        );
+    }
+
+
+    private getIconSetups(options:Array<ComponentFramework.PropertyHelper.OptionMetadata>, icons:Array<string>) : Array<IIconSetup>
+	{
+		return options.map((option,index)=>this.getIconSetup(option,icons[index]));
+	}
+
+	private getIconSetup(option:ComponentFramework.PropertyHelper.OptionMetadata, icon:string) : IIconSetup
+	{
+		return {key:option.Value,icon:icon,text:option.Label};
+	}
+
+
+    /**
+     * It is called by the framework prior to a control receiving new data.
+     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+     */
+    public getOutputs(): IOutputs {
+        return {
+			optionset: this._selected
+		};
+    }
+
+    /**
+     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+     * i.e. cancelling any pending remote calls, removing listeners, etc.
+     */
+    public destroy(): void {
+        // Add code to cleanup control if necessary
+    }
+}
